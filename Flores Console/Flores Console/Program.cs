db9fff50b9512087058633fa85d3af62ede8ff33
ElianaLuporiL <136008734+ElianaLuporiL@users.noreply.github.com>
2023-06-08 22:15:09 -0300

    Update Program.cs

--- Flores Console/Flores Console/Program.cs
+++ Flores Console/Flores Console/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hola, mi nombre es Luisina");
+
+public class Asignatura 
+{
+  public string Dia {get;set;}
+  public string Nombre {get;set;}
+  public string Profesor {get;set;}
+  public float CargaHoraria {get;set;}
+}
+  
